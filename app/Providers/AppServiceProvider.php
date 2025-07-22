d7c626f31713ed0c5d0ebdb2e42f7f38f72517e3
RivaldoZ09 <155010659+riky09mr@users.noreply.github.com>
2025-07-22 15:12:13 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Constants\SystemConstants;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
